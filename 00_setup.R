31f7659acccb3f13c2bd2729d4cb8d64046506ee
stevecrawshaw <steve.crawshaw@bristol.gov.uk>
2021-09-20 22:06:47 +0100

    first

---
+++ 00_setup.R
@@ -0,0 +1,29 @@
+packages <- c("config", "fastverse", "tidyverse", "here", "janitor", "httr", "jsonlite", "DatawRappr", "glue")
+library(xfun)
+pkg_attach2(packages)
+# devtools::install_github("munichrocker/DatawRappr")
+library(DatawRappr)
+
+apikey <- get(file = "credentials/config.yml",
+              config = "default",
+              value = "access_token")
+
+user <- get(file = "credentials/config.yml",
+            config = "default",
+            value = "user")
+
+datawrapper_auth(api_key = apikey)
+dw_get_api_key()
+
+config <- list(
+  add_headers(Authorization =  glue("Bearer {apikey}"))
+)
+
+check_url <- "https://api.datawrapper.de/v3/me/"
+
+r <- GET(check_url, add_headers(Authorization =  glue("Bearer {apikey}")))
+
+content(r, as = "text") %>% 
+  fromJSON() %>% 
+  unlist() %>% 
+  enframe() 
